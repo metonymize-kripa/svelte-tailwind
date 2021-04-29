e8189998b2e3170995d6f73a0e831bf17a0cca92
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-29 01:15:02 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -34,7 +34,7 @@
 
 <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
   <div class="flex-shrink-0">
-    <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+    <img class="h-12 w-12" src=".favicon.png" alt="ChitChat Logo">
   </div>
   <div>
     <div class="text-xl font-medium text-black">ChitChat</div>
